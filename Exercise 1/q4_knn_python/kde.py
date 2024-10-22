a5f067de3f0be87dada4ca29dbb4b22531e56368
marius-ad <marius@adamske.de>
2024-10-22 23:36:09 +0200

    push E1 into git

---
+++ Exercise 1/q4_knn_python/kde.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+def kde(samples, h):
+    """
+    compute density estimation from samples with KDE and a Gaussian kernel
+    Input
+     samples    : (N,) vector of data points
+     h          : standard deviation of the Gaussian kernel
+    Output
+     estimatedDensity : (200, 2) matrix of estimated density in the range of [-5, 5]
+    """
+
+    # Get the number of the samples x_n
+    N = len(samples)
+
+    # Create a linearly spaced vector of 200 points between -5 and 5
+    pos = np.arange(-5.0, 5.0, 0.05)  # "x"
+
+    #####Insert your code here for subtask 5a#####
+    # Estimate the density from the samples
+
+    # define kernel method, here Gaussian-dist.
+    def kernel(x) -> float:
+        return (1 / np.sqrt(2 * np.pi)) * np.exp(-(x ** 2) / 2)
+
+    # define pdf
+    def pdf(x) -> float:
+        kernelSum = 0
+        for xSample in samples:
+            kernelSum += kernel((x - xSample) / h)
+
+        return kernelSum / (N * h)  # normalize
+
+    # estimate density for each position
+    estimate = np.zeros(200)
+    for i in range(len(pos)):
+        estimate[i] = pdf(pos[i])
+
+    """
+    ### faster way with numpy vector operations ###
+    
+    # calculate difference between pos and each sample (with numpy broadcasting)
+    diff = (pos.reshape(200, 1) - samples.reshape(1, 100)) / h
+    # calculate kernel values
+    kernel_values = kernel(diff)
+    # build the sum of kernel values for each position and normilize
+    estimate = np.sum(kernel_values, axis=1) / (N * h)
+    """
+
+    # Form the output variable
+    estimatedDensity = np.stack((pos, estimate), axis=1)
+
+    return estimatedDensity
+
+
+
---
+++ Exercise 1/q4_knn_python/knn.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+def knn(samples, k):
+    """
+    compute density estimation from samples with k-NN
+    Input
+     samples    : (N,) vector of data points
+     k          : number of neighbors
+    Output
+     estimatedDensity : (200, 2) estimated density in the range of [-5, 5]
+    """
+
+    #####Insert your code here for subtask 5b#####
+    N = len(samples)
+    pos = np.arange(-5.0, 5.0, 0.05)
+    samples.sort()
+
+    def pdf(V):
+        return k / (N * V)
+
+    # reshape for broadcasting
+    broadPos = pos.reshape(200, 1)
+    broadSamples = samples.reshape(1, 100)
+
+    # calc distance between positions and each sample and sort
+    distance = np.abs(broadPos - broadSamples)
+    distance = np.sort(distance, axis=1)
+
+    # take for each pos the kth nearest neighbor
+    kth_neighbor_distance = distance[:, k-1]
+
+    estimate = pdf(kth_neighbor_distance)
+
+    # normalize the density function -> integral must be 1
+    delta_x = 0.05
+    integral = np.sum(estimate) * delta_x
+    estimate /= integral
+
+    # Form the output variable
+    estimatedDensity = np.stack((pos, estimate), axis=1)
+
+    return estimatedDensity
