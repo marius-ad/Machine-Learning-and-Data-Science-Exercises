1ffa73e4e345fa7ccfb07b250730ba07683be5cd
marius-ad <marius@adamske.de>
2024-10-27 23:15:19 +0100

    push E2 into git

---
+++ Exercise 2/q2_expectation_maximization_python/getLogLikelihood.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def getLogLikelihood(means, weights, covariances, X):
+    # Log Likelihood estimation
+    #
+    # INPUT:
+    # means          : Mean for each Gaussian KxD
+    # weights        : Weight vector for K Gaussians
+    # covariances    : Covariance matrices for each gaussian DxDxK
+    # X              : Input data NxD
+    # where N is number of data points
+    # D is the dimension of the data points
+    # K is number of gaussians
+    #
+    # OUTPUT:
+    # logLikelihood  : Log-likelihood (a scalar)
+
+    #####Insert your code here for subtask 6a#####
+    return logLikelihood
+
